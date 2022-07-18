9a2d4d408db32562cb76dd960c323380ffa22fbf
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-07-18 03:33:45 +0000

    deleted methods from interface

--- events/Events.go
+++ events/Events.go
@@ -4,13 +4,13 @@ import (
 	"context"
 
 	"github.com/mrpiggy97/cqrs/models"
+	"github.com/nats-io/nats.go"
 )
 
 type EventStore interface {
 	Close()
 	PublishCreatedFeed(cxt context.Context, feed *models.Feed) error
-	SubscribeCreatedFeed(cxt context.Context) (<-chan CreatedFeedMessage, error)
-	OnCreateFeed(callback func(CreatedFeedMessage)) error
+	StartSubscribing(natsMessageHandler nats.MsgHandler) error
 }
 
 var eventStore EventStore
@@ -27,10 +27,6 @@ func PublishCreatedFeed(cxt context.Context, feed *models.Feed) error {
 	return eventStore.PublishCreatedFeed(cxt, feed)
 }
 
-func SubscribeCreatedFeed(cxt context.Context) (<-chan CreatedFeedMessage, error) {
-	return eventStore.SubscribeCreatedFeed(cxt)
-}
-
-func OnCreateFeed(callback func(CreatedFeedMessage)) error {
-	return eventStore.OnCreateFeed(callback)
+func StartSubscribing(natsMessageHandler nats.MsgHandler) error {
+	return eventStore.StartSubscribing(natsMessageHandler)
 }
